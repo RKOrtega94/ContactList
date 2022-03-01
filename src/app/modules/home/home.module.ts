873347555c28cca4e914bb9e7679a9651259a63c
RKOSysdev <rkortega1994@gmail.com>
2022-03-01 08:56:49 -0500

    Add list view

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './page/home/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ListComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export class HomeModule {}
